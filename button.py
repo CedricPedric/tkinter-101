7b9738eb834066e2c661c395d3d9ae7375deadbc
CedricPedric <99059069@mydavinci.nl>
2021-11-10 11:22:06 +0100

    Grabbelton

--- button.py
+++ button.py
@@ -2,11 +2,18 @@ from math import radians
 import random
 import tkinter
 
+def makeButton():
+    button = tkinter.Button(root)
+    button.configure(text='You will get a pokemon!',command=updateWindow)
+    button.pack(padx=50,pady=50,expand=True)
 
+randomItems = ['Bulbasaur','Charmander','Squirtle','Pikachu','Scorbunny','Axel','Raichu','Glalie','Dragonair','Leafon']
 def updateWindow():
-    randomItems = ['Bulbasaur','Charmander','Squirtle']
+    if len(randomItems) == 0:
+        root.destroy()
+        return
     randomItem = random.choice(randomItems)
-    button.destroy()
+    randomItems.remove(randomItem)
     reward = tkinter.Label(root, text = 'You got ' + randomItem,)
     if randomItem == 'Bulbasaur':
         reward['background'] = 'green'
@@ -14,14 +21,18 @@ def updateWindow():
         reward['background'] = 'red'
     elif randomItem == 'Squirtle':
         reward['background'] = 'blue'
-    reward.pack(ipadx=100,ipady=100,expand=True)
+    else:
+        reward['background'] = 'yellow'
+    reward.pack(ipadx=20,ipady=20)
+
+
 
 root = tkinter.Tk()
-
 root.configure(bg='gray')
 
 button = tkinter.Button(root)
-button.configure(text='Choose a pokemon!',command=updateWindow)
-button.pack(padx=20,pady=20)
+button.configure(text='You will get a pokemon!',command=updateWindow)
+button.pack(padx=50,pady=50,expand=True)
+
 
 root.mainloop()
